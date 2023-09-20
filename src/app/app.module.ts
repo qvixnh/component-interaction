543fc85b3e9fc7553a44ddb66643df0d7e289cbc
qvixnh <nguyenvinh22012002@gmail.com>
2023-09-20 14:37:04 +0700

    Pass data from parent to child with input binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child.component';
+import { HeroParentComponent } from './hero-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroChildComponent,
+    HeroParentComponent,
   ],
   imports: [
     BrowserModule
