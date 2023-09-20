8439ec7cbd9ca53b61338d4d0dfd9eb2729ad83e
qvixnh <nguyenvinh22012002@gmail.com>
2023-09-20 14:46:38 +0700

    Intercept input property changes with a setter ('Master')

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroChildComponent } from './hero-child.component';
 import { HeroParentComponent } from './hero-parent.component';
+import { NameParentComponent } from './name-parent.component';
+import { NameChildComponent } from './name-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroChildComponent,
     HeroParentComponent,
+    NameParentComponent,
+    NameChildComponent
   ],
   imports: [
     BrowserModule
